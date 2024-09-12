5698cb3e40accd0d3bf521205ce2e7d7f6a04c3e
cheshiry <agnesdacunha02@gmail.com>
2024-09-12 10:02:49 -0300

    aula02

---
+++ index.js
@@ -0,0 +1,20 @@
+const star = () => {
+    let count = 0;
+    while(true){
+        let opcao = "cadastrar";
+        
+        switch (opcao) {
+            case "cadastar":
+                console.log("Vamos cadastar")
+                break;
+        
+            case "listar":
+
+                break;
+            default:
+                break;
+        }
+    }
+}
+
+star();
