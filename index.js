9a7594d56b67114554766769b12fcb5757d8c9c2
Cheshiry <158404481+Cheshiry@users.noreply.github.com>
2024-09-12 11:14:38 -0300

    Atualizar o index.js
    aulas 02 parte 13min

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
-const star = () => {
+const start = () => {
     let count = 0;
     while(true){
         let opcao = "cadastrar";
         
         switch (opcao) {
-            case "cadastar":
+            case "sair":
                 console.log("Vamos cadastar")
                 break;
         
             case "listar":
-
+													console.log("vamos listar");
                 break;
-            default:
-                break;
+            case "sair":
+												return;
+                
         }
     }
 }
+//npm install inquirer
 
-star();
+start();
